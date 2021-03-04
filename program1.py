a843255afd83f854d9e0f53ab1ffb2692c908030
NaldoYohardi <64055099+NaldoYohardi@users.noreply.github.com>
2021-03-04 15:13:44 +0700

    user input till certain prompt

--- program1.py
+++ program1.py
@@ -1,10 +1,13 @@
+while (a != 'x'):
 a = int(input("Masukkan Angka : "))
-
 if a < 0:
   print("Angka Negatif")
 elif a > 0:
   print("Angka Positif")
 elif a == 0:
   print("Angka yang anda Masukkan nol")
+elif a == 'x':
+  print("Exit Program")
+  break
 else: 
   print("Error Input")
