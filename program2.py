06bf74ccb3662e1bb52b03476b70d382aec3a550
NaldoYohardi <64055099+NaldoYohardi@users.noreply.github.com>
2021-03-04 15:53:02 +0700

    Continuous Input

--- program2.py
+++ program2.py
@@ -1,10 +1,18 @@
-try:
-  x = int(input("Input : "))
-except:
-  print("Error - Input Integers Only !")
-if x > 0:
-  print("Positive")
-elif x < 0:
-  print("Negative")
-elif x == 0:
-  print("Zero")
+x = ''
+y = ''
+
+while (y != 'n'):
+  try:
+    x = int(input("Input : "))
+  except:
+    print("Error - Input Integers Only !")
+    continue
+  if x > 0:
+    print("Positive")
+  elif x < 0:
+    print("Negative")
+  elif x == 0:
+    print("Zero")
+    
+  y = input("Continue ? y/n : ")
+  
