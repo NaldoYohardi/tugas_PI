6c47933adcfebca5a0b90fcdbd5d3ea2f4a873f6
NaldoYohardi <64055099+NaldoYohardi@users.noreply.github.com>
2021-03-04 15:05:29 +0700

    Fix apabila yang dimasukkan bukan angka

--- program1.py
+++ program1.py
@@ -2,7 +2,9 @@ a = int(input("Masukkan Angka : "))
 
 if a < 0:
   print("Angka Negatif")
-if a > 0:
+elif a > 0:
   print("Angka Positif")
-if a == 0:
+elif a == 0:
   print("Angka yang anda Masukkan nol")
+else: 
+  print("Error Input")
