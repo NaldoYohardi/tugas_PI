23cf1a87372c5868776484b7f44f6291ff58c1a6
NaldoYohardi <64055099+NaldoYohardi@users.noreply.github.com>
2021-03-04 15:28:50 +0700

    Continuous Checking

--- program.py
+++ program.py
@@ -1,10 +1,11 @@
-try :
-  x = int(input("Input : "))
-except TypeError:
-  print("Only Integers Are Allowed")
-if x < 0:
-  print("Negative")
-if x > 0:
-  print("Positive")
-if x == 0:
-  print("Zero")
+while (True):
+  try :
+    x = int(input("Input : "))
+  except TypeError:
+    print("Only Integers Are Allowed")
+  if x < 0:
+    print("Negative")
+  if x > 0:
+    print("Positive")
+  if x == 0:
+    print("Zero")
